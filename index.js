1ebb5051d092edef53b74b8dae8f0eb82668ffa0
weaponsforge <forgeweapons@gmail.com>
2019-10-30 18:44:43 +0800

    Updated message.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.get('/', (req, res) => {
-  res.status(200).send('It works!')
+  res.status(200).send({message: 'It Works!'})
 })
 
 app.listen(PORT, () => {
